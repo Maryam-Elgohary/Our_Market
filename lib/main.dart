da620250c73c7a87a54142986970fa9e48d2be74
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-23 08:15:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:our_market/views/auth/UI/login_view.dart';
+
+void main() {
+  runApp(const OurMarket());
+}
+
+class OurMarket extends StatelessWidget {
+  const OurMarket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Our Market',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
