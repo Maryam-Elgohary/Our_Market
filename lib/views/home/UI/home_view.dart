357b4f57227976b3605293ef8a8135ea2a4d98fc
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-23 13:09:27 +0200

    create product card and add network image in it

--- lib/views/home/UI/home_view.dart
+++ lib/views/home/UI/home_view.dart
@@ -16,14 +16,41 @@ class HomeView extends StatelessWidget {
             height: 20,
           ),
           Image.asset("assets/buy.jpg"),
-          Text(
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
             "Popular Categories",
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           const SizedBox(
-            height: 15,
+            height: 20,
           ),
-          const CategoriesList()
+          const CategoriesList(),
+          const SizedBox(
+            height: 20,
+          ),
+          const Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          bottomLeft: Radius.circular(16)),
+                      child: Image(
+                          image: NetworkImage(
+                              "https://img.freepik.com/premium-psd/kitchen-product-podium-display-background_1101917-13418.jpg?w=900")),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
