d0a72ce447f5ed7871e8b43f6fd38fc170d09352
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-23 15:46:56 +0200

    create products card listview & finish home view UI

---
+++ lib/core/components/prodcts_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:our_market/core/components/product_card.dart';
+
+class ProductsList extends StatelessWidget {
+  const ProductsList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const ProductCard();
+        });
+  }
+}
