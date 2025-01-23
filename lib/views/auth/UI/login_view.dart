f92e289e7ee9e6cd348eb41ad4a246930be4a132
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-23 09:24:32 +0200

    create custom button

--- lib/views/auth/UI/login_view.dart
+++ lib/views/auth/UI/login_view.dart
@@ -43,12 +43,48 @@ class LoginView extends StatelessWidget {
                     icon: const Icon(Icons.visibility_off),
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CustomTextButton(
+                      text: 'Forget Password?',
+                      onTap: () {},
+                    ),
+                  ],
+                )
               ],
             ),
           ),
         )
       ],
     )));
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    super.key,
+    this.onTap,
+    required this.text,
+  });
+  final void Function()? onTap;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: TextStyle(
+            color: AppColors.kPrimaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+      ),
+    );
   }
 }
 
