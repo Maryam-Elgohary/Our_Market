53a5955563b8ad375eb3ea73a283e42fdc00d3f9
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-23 12:53:32 +0200

    add categories list to home view

--- lib/views/home/UI/home_view.dart
+++ lib/views/home/UI/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/core/components/custom_search_field.dart';
 
 class HomeView extends StatelessWidget {
@@ -14,9 +15,52 @@ class HomeView extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          Image.asset("assets/buy.jpg")
+          Image.asset("assets/buy.jpg"),
+          Text(
+            "Popular Categories",
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+              height: 100,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                              radius: 30,
+                              backgroundColor: AppColors.kPrimaryColor,
+                              foregroundColor: AppColors.kWhiteColor,
+                              child: Icon(categories[index].icon, size: 40)),
+                          Text(categories[index].text)
+                        ],
+                      ),
+                    );
+                  }))
         ],
       ),
     );
   }
+}
+
+List<Category> categories = [
+  Category(icon: Icons.sports, text: "Sports"),
+  Category(icon: Icons.tv, text: "Electronics"),
+  Category(icon: Icons.collections, text: "Collections"),
+  Category(icon: Icons.book, text: "Books"),
+  Category(icon: Icons.games, text: "Games"),
+  Category(icon: Icons.bike_scooter, text: "Bikes"),
+];
+
+class Category {
+  final IconData icon;
+  final String text;
+
+  Category({required this.icon, required this.text});
 }
