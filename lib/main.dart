b3fe2f83b7abc21514338cdfe547a00a6f6d7fa3
Maryam Elgohary <maryamelgohary4@gmail.com>
2025-01-28 11:52:49 +0200

    Initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/views/nav_bar/UI/main_home_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://adfznwtttqzsiuzjvyun.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZnpud3R0dHF6c2l1emp2eXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNTczMDYsImV4cCI6MjA1MzYzMzMwNn0.M9tetd94urVsetYBWQPD3byC488acxo9C7W_fJqxhBM',
+  );
   runApp(const OurMarket());
 }
 
